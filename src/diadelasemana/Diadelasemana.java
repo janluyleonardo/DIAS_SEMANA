8b9479a8d8ca2615d086d2a44bbf9b5a5a13972c
janluyleonardo <janluy.moreno@gmail.com>
2025-08-12 12:25:07 -0500

    carga dearchivos adicionales

---
+++ src/diadelasemana/Diadelasemana.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diadelasemana;
+
+/**
+ *
+ * @author 15_TECFON2
+ */
+public class Diadelasemana {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Interfaz in = new Interfaz();
+        in.setVisible(true);
+        in.setLocationRelativeTo(null);
+    }
+    
+}
